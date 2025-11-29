1a9f33693e2beeb65ff19229fef237119c7a1ef9
Soujiro0 <castrudesjerry11@gmail.com>
2025-11-30 04:10:31 +0800

    change Router to BrowserRouter with basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './routes/AppRoute' // Import AppRoutes
 import { GameProvider } from './contexts/GameContext'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter basename='market-pulse'>
     <GameProvider>
       <AppRoutes /> {/* Render AppRoutes */}
     </GameProvider>
-  </StrictMode>,
+  </BrowserRouter>
 )
--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,22 +1,20 @@
 // src/routes/AppRoute.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import App from '../App'; // App will be our layout component
 import { HomePage, MarketPage, TradingPage, SimulationPage, ProfilePage, HelpPage } from '../pages';
 
 const AppRoutes = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<App />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="market" element={<MarketPage />} />
-                    <Route path="trading" element={<TradingPage />} />
-                    <Route path="simulation" element={<SimulationPage />} />
-                    <Route path="profile" element={<ProfilePage />} />
-                    <Route path="help" element={<HelpPage />} />
-                </Route>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route index element={<HomePage />} />
+                <Route path="market" element={<MarketPage />} />
+                <Route path="trading" element={<TradingPage />} />
+                <Route path="simulation" element={<SimulationPage />} />
+                <Route path="profile" element={<ProfilePage />} />
+                <Route path="help" element={<HelpPage />} />
+            </Route>
+        </Routes>
     );
 };
 
