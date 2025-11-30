ee78409a0f1766434bff614d07d4b6295e649f70
Soujiro0 <castrudesjerry11@gmail.com>
2025-11-30 16:22:07 +0800

    Displaying version in Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,11 @@
+import packageJson from '../../../package.json';
+
 const Footer = () => {
+    const version = JSON.stringify(packageJson.version);
     return (
         <footer className="bg-slate-900 border-t border-slate-800 py-6 mt-auto">
             <div className="max-w-7xl mx-auto px-4 flex justify-center items-center text-slate-500 text-xs font-mono">
-                <p>MARKET PULSE_SIMULATION.SYS // V.2.8.4 // AUTHORIZED_USER_ONLY</p>
+                <p>v{version.replace(/"/g, '')}</p>
             </div>
         </footer>
     );
