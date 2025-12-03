395c7bac8a0796676517b7a2f55374e2a828f349
Jerryyy <150907918+Soujiro0@users.noreply.github.com>
2025-12-03 21:03:26 +0800

    Replace HashRouter with BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './routes/AppRoute' // Import AppRoutes
 import { GameProvider } from './contexts/GameContext'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename='market-pulse'>
+  <BrowserRouter basename='market-pulse'>
     <GameProvider>
       <AppRoutes /> {/* Render AppRoutes */}
     </GameProvider>
-  </HashRouter>
+  </BrowserRouter>
 )
